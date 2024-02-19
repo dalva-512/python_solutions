974e40dfa17c72768c95d3efb85c1a514b2fdc13
Deeksha Alva <157198236+dalva-512@users.noreply.github.com>
2024-02-19 18:48:58 +0530

    Create contains_dup_lc.py

---
+++ contains_dup_lc.py
@@ -0,0 +1,30 @@
+"""
+Contains Duplicate:
+====================
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct. 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+"""
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dct = dict()
+        for i in range(len(nums)):
+            cnt = nums.count(nums[i])
+            dct[i] = cnt
+    
+        for i in dct.values():
+            if i >= 2:
+                return True
+        else:
+            return False
