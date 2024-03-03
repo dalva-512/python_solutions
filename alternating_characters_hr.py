6a19a0a632ab23b9a42b0a830272fe99d4dcc143
Deeksha Alva <157198236+dalva-512@users.noreply.github.com>
2024-03-03 20:40:20 +0530

    Update alternating_characters_hr.py

--- alternating_characters_hr.py
+++ alternating_characters_hr.py
@@ -1,5 +1,5 @@
 """
-String Manipulation: Making Anagrams
+String Manipulation: Alternating Characters
 You are given a string containing characters  and  only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
 
 Your task is to find the minimum number of required deletions.
