5ad00bad05264ee0b7dbb566292c6df1e4941242
Deeksha Alva <157198236+dalva-512@users.noreply.github.com>
2024-03-03 20:38:38 +0530

    Create alternating_characters_hr.py
    Hackerrank Solution

---
+++ alternating_characters_hr.py
@@ -0,0 +1,83 @@
+"""
+String Manipulation: Making Anagrams
+You are given a string containing characters  and  only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
+
+Your task is to find the minimum number of required deletions.
+
+Example
+s = "AABAAB"
+
+Remove an A at positions 0 and 3 to make s=ABAB in 2 deletions.
+
+Function Description
+
+Complete the alternatingCharacters function in the editor below.
+
+alternatingCharacters has the following parameter(s):
+
+string s: a string
+Returns
+int: the minimum number of deletions required
+Input Format
+
+The first line contains an integer , the number of queries.
+The next  lines each contain a string  to analyze.
+
+Constraints
+
+Each string  will consist only of characters  and .
+Sample Input
+
+5
+AAAA
+BBBBB
+ABABABAB
+BABABA
+AAABBB
+Sample Output
+
+3
+4
+0
+0
+4
+Explanation
+
+The characters marked red are the ones that can be deleted so that the string does not have matching adjacent characters.
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    cnt = 0
+    for i in range(len(s) - 1):
+        j = i + 1
+        if s[i] == s[j]:
+            cnt += 1
+    return cnt
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
