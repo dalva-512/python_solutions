b300e81ce9b6b1b9847ea46a3efd3891738edf10
Deeksha Alva <157198236+dalva-512@users.noreply.github.com>
2024-03-18 19:11:03 +0530

    Create sort_array_int_geeks.py

---
+++ sort_array_int_geeks.py
@@ -0,0 +1,15 @@
+"""
+Sort an array of 0s, 1s and 2s:
+================================
+Sort an array of 0s, 1s and 2s | Dutch National Flag problem:
+Given an array A[] consisting of only 0s, 1s, and 2s. The task is to write a function that sorts the given array. The functions should put all 0s first, then all 1s and all 2s in last.
+
+This problem is also the same as the famous “Dutch National Flag problem”. The problem was proposed by Edsger Dijkstra. The problem is as follows:
+
+Given N balls of colour red, white or blue arranged in a line in random order. You have to arrange all the balls such that the balls with the same colours are adjacent with the order of the balls, with the order of the colours being red, white and blue (i.e., all red coloured balls come first then the white coloured balls and then the blue coloured balls). 
+"""
+def sort(arr):
+    return sorted(arr)
+
+final_arr = sort(arr)
+print(f"Sorted array is: {final_arr}")
